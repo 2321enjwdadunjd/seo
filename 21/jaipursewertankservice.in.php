36d41932b8cd3337c4c012bd9d34b77f50d71a92
2321enjwdadunjd <132290011+2321enjwdadunjd@users.noreply.github.com>
2023-06-01 05:51:42 +0700

    Add files via upload

---
+++ 21/jaipursewertankservice.in.php
@@ -0,0 +1,329 @@
+hei
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+    <title>Jaipur Sewar Tank Services</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8" />
+    <meta name="keywords" content="Jaipur Sewar Tank Services, Sewer tank in Jaipur" />
+    <script>
+        addEventListener("load", function () {
+            setTimeout(hideURLbar, 0);
+        }, false);
+
+        function hideURLbar() {
+            window.scrollTo(0, 1);
+        }
+    </script>
+
+<style>
+.aSlides {display:none;}
+</style>
+    <!-- Custom Theme files -->
+    <link href="css/bootstrap.css" type="text/css" rel="stylesheet" media="all">
+    <link href="css/style.css" type="text/css" rel="stylesheet" media="all">
+    <!-- font-awesome icons -->
+    <link href="css/fontawesome-all.min.css" rel="stylesheet">
+    <!-- //Custom Theme files -->
+    <!-- online-fonts -->
+    <link href="//fonts.googleapis.com/css?family=EB+Garamond:400,400i,500,500i,600,600i,700,700i,800,800i" rel="stylesheet">
+    <!-- //online-fonts -->
+</head>
+
+<body> <!-- header -->
+<script>    
+$("#slideshow > div:gt(0)").hide();
+
+setInterval(function() { 
+  $('#slideshow > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshow');
+},  3000);
+</script>
+
+          <div class="col-md-12">
+             
+       <header>
+            <nav class="navbar navbar-expand-lg navbar-light bg-gradient-secondary pt-3" style="    background-color: #fffa9c;">
+
+            
+                     <a class="navbar-brand" href="index.php">
+                      <h5 class="card-title2" style="color: #01933d;    padding-top: 28px;">JAIPUR SEWAR TANK SERVICES</h5>
+            </a>
+                <button class="navbar-toggler ml-md-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ml-lg-auto text-center">
+                        <li class="nav-item active  mr-lg-3">
+                            <a class="nav-link" href="index.php">Home
+                                <span class="sr-only">(current)</span>
+                            </a>
+                        </li>
+                        <li class="nav-item active  mr-lg-3">
+                            <a class="nav-link" href="about.php">about</a>
+                        </li>
+                        <li class="nav-item active  mr-lg-3">
+                            <a class="nav-link" href="services.php">Service</a>
+                        </li>
+                        <li class="nav-item  active mr-lg-3">
+                            <a class="nav-link" href="gallery.php">Enquery</a>
+                        </li>
+                       
+                        <li class="nav-item active">
+                            <a class="nav-link" href="contact.php">contact</a>
+                        </li>
+                      
+                    </ul>
+                </div>
+
+            </nav>
+        </header>
+        </div>
+        <!-- //header -->
+    <!-- //header -->
+    <!-- banner -->
+   
+<div id="slideshow">
+   <div id="slideshow">
+     <img  src="image/banner/jaipur-sewer-tank-service-Banner.jpg" class="bannersize">
+   </div>
+   <div>
+   <!--  <img src="image/banner/jaipur-sewer-tank-service-Banner.jpg" style="width: 100%;height: 600px;">-->
+   </div>
+   <div>
+    
+   </div>
+</div>
+
+   
+    <!--/banner-section-->
+   
+    <!-- about -->
+   <!-- <section class="wthree-row py-lg-5 pt-sm-5 pt-3 about-main">
+        <div class="container py-lg-5">
+            <div class="pt-lg-5 bg-pricemain row">
+                <div class="col-lg-4">
+                    <h3 class="agile-title text-uppercase">heading goes here</h3>
+                    <span class="w3-line"></span>
+                    <h5 class="agile-title text-capitalize pt-4">Just add your content and you're ready to go! </h5>
+                    <p class="py-4">Aliquam ac est vel nisl condimentum interduauris et justo vel nibh rhoncusm vel eget enim. Curabitur
+                        mattis orci sed leo mattis, nec maximus nibh faucibus. Mauris et justo vel nibh rhoncus venenatis.
+                        Null est vel nisl condimentum interduauris et justo al condimentum interdum vel eget enim. Curabitur
+                        mattis orci sed le.
+                    </p>
+                    <a href="services.php" class="serv_link align-self-center serv_bottom btn px-4 text-white">Read more</a>
+                </div>
+                <div class="col-lg-4 my-lg-0 my-3">
+                    <div class="agileits-banner-info4">
+                        <h3>p</h3>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <i class="fas fa-quote-left" aria-hidden="true"></i>
+                    <p class="py-4 pl-3 d-inline font-italic text-dark about-right-text">Aliquam ac est vel nisl condimentum interdum vel eget enim. Curabitur mattis orci sed leo mattis, nec
+                        maximus nibh faucibus. Mauris et justo vel nibh rhoncus venenatis. Nullal condimentum interdum vel
+                        eget enim. Curabitur mattis orci sed le.
+                    </p>
+                    <h5 class="abt-right">prime</h5>
+                    <hr>
+                    <h4 class="abt-right-text font-italic">the best interior solution.</h4>
+                </div>
+            </div>
+        </div>
+    </section>  -->
+    <!-- //about -->
+    <!-- services -->
+    <section class="wthree-row  py-5">
+        <div class="container py-md-5">
+            <h3 class="agile-title text-uppercase">Services We Offer</h3>
+            <span class="w3-line"></span>
+            <div class="row py-lg-5 pt-3 d-flex justify-content-center">
+                <div class="card col-lg-4 border-0 mt-lg-0 mt-3">
+                    <img class="card-img-top" src="image/septictankservice1.jpg" alt="Card image cap ">
+                    <div class="card-body text-center">
+                        <h5 class="card-title">Septic Tank Cleaning</h5>
+                        <p class="card-text mb-4">We are the best residential and commercial septic tank cleaning company in jaipur . we are a preferred septic tank cleaning service provider in jaipur at affordable price. so call us ASAP.</p></br>
+                        <button type="button" class="btn serv_bottom w3ls-btn text-white" data-toggle="modal" aria-pressed="false" data-target="#exampleModal">
+                                Enquiry
+                            </button>
+                    </div>
+                </div>
+                <div class="card col-lg-4 border-0 mt-lg-0 mt-5">
+                    <img class="card-img-top" src="image/11294033.jpg" alt="Card image cap ">
+                    <div class="card-body text-center">
+                        <h5 class="card-title">Sewar Tank Maintenance</h5>
+                        <p class="card-text mb-4">Our sewer and drain cleaning crews will clear any wastage. We offer very cost effective solutions for your Drain Cleaning, Sewage Spills and Septic Tank Cleaning.</p>
+                        <button type="button" class="btn serv_bottom  w3ls-btn text-white" data-toggle="modal" aria-pressed="false" data-target="#exampleModal">
+                                Enquiry
+                            </button>
+                    </div>
+                </div>
+                <div class="card col-lg-4  border-0 mt-lg-0 mt-5">
+                    <img class="card-img-top" src="image/surya-septic-tank-cleaning-service-pathanamthitta-026zzzstml.jpg" alt="Card image cap ">
+                    <div class="card-body text-center">
+                        <h5 class="card-title ">Industrial Tank Cleaning Services</h5>
+                        <p class="card-text mb-4">Our team is always ready for cleaning services for Hotels, Lodges, Marriage Halls, Schools, Colleges, Dyeing Units, Industries, etc.</p></br>
+                        <button type="button" class="btn serv_bottom  w3ls-btn text-white" data-toggle="modal" aria-pressed="false" data-target="#exampleModal">
+                                Enquiry
+                            </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <div class="row  no-gutters abbot-main">
+        <div class="col-lg-6 p-0">
+            <img src="image/septic-tank-maintenance.jpg" class="img-fluid" alt="">
+        </div>
+        <div class="col-lg-6 abbot-right">
+            <h3 class="stat-title card-title align-self-center">About us Jaipur Septic Tank Cleaning service</h3>
+            <span class="w3-line"></span>
+            <p class="card-text align-self-center my-lg-5 mt-3" style="font-size: 18px;">
+              Jaipur Septic Tank Cleaning service are a one stop solution for all your cleaning needs and offer a variety of services including commercial, residential, corporate Building, etc. Septic Tank Cleaning Services in Jaipur. We offers cleaning and desalting the domestic and industrial sewage wastes.  </p><p style="font-size: 18px;">
+We are ensuring Quality and standard service through our fully trained operators & updated machinery and equipment.  so call us for cleaning needs..we will help you...</p>
+           
+        </div>
+     <!--   <div class="agileits-services-row serv_bottom">
+            <div class="text-center">
+                <div class="row no-gutters">
+                    <div class="col-6  agileits-services-grids border-right">
+                        <span class="fab fa-servicestack p-4"></span>
+                        <h4>Vestibulum</h4>
+                    </div>
+                    <div class="col-6 agileits-services-grids">
+                        <span class="far fa-compass p-4"></span>
+                        <h4>Loremipsum</h4>
+                    </div>
+                    <div class="col-6 agileits-services-grids border-left-0 border">
+                        <span class="fas fa-globe p-4"></span>
+                        <h4>Vestibulum</h4>
+                    </div>
+                    <div class="col-6 agileits-services-grids border-top border-bottom">
+                        <span class="fas fa-magic p-4"></span>
+                        <h4>NullaAtqui</h4>
+                    </div>
+                    <div class="col-6 agile_service_bottom agileits-services-grids border-right">
+                        <span class="fas fa-chart-pie p-4"></span>
+                        <h4>Refiniti</h4>
+                    </div>
+                    <div class="col-6  agileits-services-grids">
+                        <span class="fab fa-servicestack p-4"></span>
+                        <h4>Oncludature</h4>
+                    </div>
+                </div>
+            </div>
+        </div>-->
+    </div>
+    <div class="about-w3sec py-sm-5">
+        <div class="container py-5">
+            <div class="row">
+                <div class="col-md-2"></div>
+                <div class="col-md-6">
+                 <img src="image/922.png" class="indexpng" alt="image" >
+                 </div>
+               <div class="col-md-4"></div>
+            </div>
+        </div>
+    </div>
+    <!-- //services -->
+    <!-- newsletter-->
+  
+    <!-- //newsletter-->
+    <!--footer -->
+     <?php include'footer.php'; ?>
+
+    <!-- js -->
+    <script src="js/jquery-2.2.3.min.js"></script>
+    <!-- //js -->
+    <!-- script for password match -->
+    <script>
+        window.onload = function () {
+            document.getElementById("password1").onchange = validatePassword;
+            document.getElementById("password2").onchange = validatePassword;
+        }
+
+        function validatePassword() {
+            var pass2 = document.getElementById("password2").value;
+            var pass1 = document.getElementById("password1").value;
+            if (pass1 != pass2)
+                document.getElementById("password2").setCustomValidity("Passwords Don't Match");
+            else
+                document.getElementById("password2").setCustomValidity('');
+            //empty string means no validation error
+        }
+    </script>
+    <!-- script for password match -->
+    <!-- contact validation js -->
+    <script src="js/form-validation.js"></script>
+
+    <!-- Responsiveslides -->
+    <script src="js/responsiveslides.min.js"></script>
+    <script>
+        // You can also use "$(window).load(function() {"
+        $(function () {
+            // Slideshow 4
+            $("#slider3").responsiveSlides({
+                auto: false,
+                pager: true,
+                nav: false,
+                speed: 500,
+                namespace: "callbacks",
+                before: function () {
+                    $('.events').append("<li>before event fired.</li>");
+                },
+                after: function () {
+                    $('.events').append("<li>after event fired.</li>");
+                }
+            });
+
+        });
+    </script>
+    <!-- // Responsiveslides -->
+    <!-- start-smooth-scrolling -->
+    <script src="js/move-top.js"></script>
+    <script src="js/easing.js"></script>
+    <script>
+        jQuery(document).ready(function ($) {
+            $(".scroll").click(function (event) {
+                event.preventDefault();
+
+                $('html,body').animate({
+                    scrollTop: $(this.hash).offset().top
+                }, 1000);
+            });
+        });
+    </script>
+    <!-- //end-smooth-scrolling -->
+    <!-- smooth-scrolling-of-move-up -->
+    <script>
+        $(document).ready(function () {
+            /*
+            var defaults = {
+                containerID: 'toTop', // fading element id
+                containerHoverID: 'toTopHover', // fading element hover id
+                scrollSpeed: 1200,
+                easingType: 'linear' 
+            };
+            */
+
+            $().UItoTop({
+                easingType: 'easeOutQuart'
+            });
+
+        });
+    </script>
+    <script src="js/SmoothScroll.min.js"></script>
+    <!-- Bootstrap core JavaScript
+================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="js/bootstrap.js"></script>
+</body>
+
+</html>
